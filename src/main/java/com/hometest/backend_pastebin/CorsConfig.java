804b69ca17d11bd904b8de37aad2b1945fa1298c
YogitaKanaki <yogitakanaki20@gmail.com>
2026-01-28 12:08:24 +0530

    Added frontend config

---
+++ src/main/java/com/hometest/backend_pastebin/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.hometest.backend_pastebin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://pastebin-lite-frontend-eight.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
+
